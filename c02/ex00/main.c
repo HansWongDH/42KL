c9252a153e4dd133ce995c888adf949452af698d
Wong Ding hang <wding-ha@u81z01s02.42kl.edu.my>
2021-02-25 17:31:44 +0800

    add c02 ex00 and ex01

---
+++ c02/ex00/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wding-ha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 11:33:50 by wding-ha          #+#    #+#             */
+/*   Updated: 2021/02/25 12:44:10 by wding-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_strcpy(char *dest, char *src);
+
+int		main(void)
+{
+	char src[6] = "HELLO";
+	char dest[6] = "WORLD";
+	char *firstsrc;
+	char *firstdest;
+
+	firstsrc = &src[0];
+	firstdest = &dest[0];
+
+	int index = 0;
+	while (index <= 5)
+	{
+		printf("%c, ", dest[index]);
+		index++;
+	}
+	ft_strcpy(firstdest, firstsrc);
+	int index2 = 0;
+	while (index2 <= 5)
+	{
+		printf("%c, ", dest[index2]);
+		index2++;
+	}
+	return (0);
+}
