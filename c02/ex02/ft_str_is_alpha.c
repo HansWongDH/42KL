50c21ee623f252f14a42cdd8833be2bf7ccc068b
Wong Ding hang <wding-ha@u81z01s02.42kl.edu.my>
2021-02-25 18:07:15 +0800

    add c02 ex02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wding-ha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 17:17:16 by wding-ha          #+#    #+#             */
+/*   Updated: 2021/02/25 18:05:02 by wding-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_str_is_alpha(char *str)
+{
+	int index;
+	int valid;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (((str[index] >= 'A' && str[index] <= 'Z'))
+		|| ((str[index] >= 'a' && str[index] <= 'z')))
+		{
+			valid = 1;
+			index++;
+		}
+		else
+		{
+			valid = 0;
+			index++;
+		}
+	}
+	return (valid);
+}
