f1c497ef1d4bf15f0937f486b8998793d8fe47c9
Wong Ding hang <wding-ha@u81z01s02.42kl.edu.my>
2021-03-12 00:27:20 +0800

    lvl8 29%. The end of my 42KL Piscine.

--- zachary/zach.h
+++
@@ -1,9 +0,0 @@
-int		g_row;
-int		g_col;
-int		g_size;
-int		validate_map(char *map_data);
-int		get_col(char *map_data);
-int		get_row(char *map_data);
-char	*read_map(int map_filedes);
-char	**write_map_arr(char **map_array, char *map_data);
-char	**allocate_map_arr(char *map_data);
